850e33410b4711ee4823a8679611c6469e4b44a6
porames <trailersubthai@gmail.com>
2017-12-06 10:30:30 +0700

    team

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="node_modules/tether/dist/css/tether.min.css" rel="stylesheet">
+        <script src="node_modules/jquery/dist/jquery.js"></script>
+        <script src="node_modules/tether/dist/js/tether.min.js"></script>
+        <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>    
+    </head>
+    <body>
+    <h1>Hello, world!</h1>
+        Git connected
+    </body>
+<?php echo "php is working" ?>
+
+</html>
