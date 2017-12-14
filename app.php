175700e3fa01261dbaa0534c76eff494c1768a31
porames <trailersubthai@gmail.com>
2017-12-14 19:24:53 +0700

    ui update

--- app.php
+++ app.php
@@ -32,6 +32,7 @@ $name = $_COOKIE['id'];
     <script src="node_modules/jquery/dist/jquery.js"></script>
     <script src="node_modules/tether/dist/js/tether.min.js"></script>
     <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="node_modules/js-cookie/src/js.cookie.js"></script>
     <link rel="stylesheet" href="node_modules/frontend/app-style.css">
     <link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet">
 
@@ -44,10 +45,10 @@ $name = $_COOKIE['id'];
             margin: auto;
             margin-bottom: 24px;
         }
+
         .inventory{
             width: 100%;
             height: auto;
-            overflow-y:auto;
         }
         .item{
             width: 100%;
@@ -75,51 +76,52 @@ $name = $_COOKIE['id'];
                 <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact"
                     aria-selected="false">Game</a>
             </li>
+
         </ul>
         <div class="tab-content" id="pills-tabContent" style="">
             <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab" style="text-align:center">
                 <div class="avatar" style="background-image:url(img/man.png)"></div>
                 Name: <?php echo $name ?>
-                <br> Group: <?php echo $group ?>
+                <br> Group: <?php echo $group ?><br>
+                <button type="button" class="logout btn btn-outline-primary" style="margin-top:20px;">Log Out</button>
             </div>
-            <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab" style="max-height: 450px; overflow: auto;">
+            <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab" >
                 <div class="inventory">
-                    <div class="slot">
-                        <div class="item"></div>
+                    <div class="row">
+                    <div class="col-sm-4">
+                        <div class="list-group" id="list-tab" role="tablist">
+                        <a class="list-group-item list-group-item-action active" id="list-1-list" data-toggle="list" href="#list-1" role="tab" aria-controls="1">Home</a>
+                        <a class="list-group-item list-group-item-action" id="list-2-list" data-toggle="list" href="#list-2" role="tab" aria-controls="2">Profile</a>
+                        <a class="list-group-item list-group-item-action" id="list-3-list" data-toggle="list" href="#list-3" role="tab" aria-controls="3">Messages</a>
+                        <a class="list-group-item list-group-item-action" id="list-4-list" data-toggle="list" href="#list-4" role="tab" aria-controls="4">Settings</a>
+                        </div>
                     </div>
-                    <div class="slot">
-                        <div class="item"></div>
+                    <div class="col-sm-8">
+                        <div class="tab-content" id="nav-tabContent">
+                        <div class="tab-pane fade show active" id="list-1" role="tabpanel" aria-labelledby="list-1-list">test</div>
+                        <div class="tab-pane fade" id="list-2" role="tabpanel" aria-labelledby="list-2-list">shit</div>
+                        <div class="tab-pane fade" id="list-3" role="tabpanel" aria-labelledby="list-3-list">banana</div>
+                        <div class="tab-pane fade" id="list-4" role="tabpanel" aria-labelledby="list-4-list">omg</div>
+                        </div>
                     </div>
-                    <div class="slot">
-                        <div class="item"></div>
-                    </div>
-                    <div class="slot">
-                        <div class="item"></div>
-                    </div>
-                    <div class="slot">
-                        <div class="item"></div>
-                    </div>
-                    <div class="slot">
-                        <div class="item"></div>
-                    </div>
-                    <div class="slot">
-                        <div class="item"></div>
-                    </div>
-                    <div class="slot">
-                        <div class="item"></div>
-                    </div>
-                    <div class="slot">
-                        <div class="item"></div>
-                    </div>
+                    </div>                          
                 </div>
 
 
 
             </div>
             <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">...</div>
+            
         </div>
 
 </body>
+<script>
+$('.logout').click(function() {
+    Cookies.remove('id');
+    window.location.href = 'index.php';
+});
+
+</script>
 <footer class="feet">
 <?php echo "php is working. (c) Pre MWITS 2018 Dev Team " ?>
 
