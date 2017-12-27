06aef7cc01e51d438a6fc572fdbab80069db6718
Flatfact.com <trailersubthai@gmail.com>
2017-12-27 13:41:17 +0700

    minor authen change

--- authen.php
+++ authen.php
@@ -15,7 +15,7 @@ $userdata = $getuser->get_result();
 $row = $userdata->fetch_array(MYSQLI_ASSOC); //all data from db in array sql injection protected
 
 if (password_verify($password, $row['pass'])) {
-    if($row['id'] == 0){
+    if($row['status'] == 'staff'){
         session_start();
         $_SESSION['id'] = $row['id'];
         header('Location: staff.php');
